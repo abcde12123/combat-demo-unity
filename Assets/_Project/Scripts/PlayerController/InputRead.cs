d16a66820defeedb63aea071ab49a13d1af72abb
tuo <18011772015@sina.cn>
2026-03-11 15:11:46 +0800

    写了第一个inputread

--- Assets/_Project/Scripts/PlayerController/InputRead.cs
+++ Assets/_Project/Scripts/PlayerController/InputRead.cs
@@ -1,23 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 //using combat;
-namespace  Inputs
+namespace Inputs
 {
-   public class InputRead : MonoBehaviour
-   {
-       // Start is called before the first frame update
-       void Start()
-       {
-           
-       }
-   
-       // Update is called once per frame
-       void Update()
-       {
-           
-       }
-   } 
+    public class InputRead : MonoBehaviour, PlayerControls.IPlayerActions
+    {
+        private PlayerControls _Controls;
+        public bool StopInput;
+
+        public Vector2 MovementOn2DAxis
+        {
+            get; 
+            private set;
+        }
+
+        public Vector2 LookOn2DAxis
+        {
+            get;
+            private set;
+        }
+        
+        public event System.Action JumpEvent;
+        public event System.Action LightAttackEvent;
+        public event System.Action DodgeEvent;
+        public event System.Action LockEvent;
+
+        void Awake()
+        {
+            _Controls = new PlayerControls();
+            _Controls.Player.SetCallbacks(this);
+            _Controls.Player.Enable();
+        }
+
+        void OnEnable()
+        {
+            _Controls.Player.Enable();
+        }
+
+        void OnDisable()
+        {
+            _Controls.Player.Disable();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            if (StopInput)
+            {
+                return;
+            }
+            MovementOn2DAxis= context.ReadValue<Vector2>();
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            LookOn2DAxis= context.ReadValue<Vector2>();
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            if(!context.performed)
+            {
+                return;
+            }
+            JumpEvent?.Invoke();
+        }
+
+        public void OnLightAttack(InputAction.CallbackContext context)
+        {
+            if (!context.performed)
+            {
+                return;
+            }
+            LightAttackEvent?.Invoke();
+        }
+
+        public void OnDodge(InputAction.CallbackContext context)
+        {
+            if (!context.performed)
+            {
+                return;
+            }
+            DodgeEvent?.Invoke();
+        }
+
+        public void OnLock(InputAction.CallbackContext context)
+        {
+            if (!context.performed)
+            {
+                return;
+            }
+            LockEvent?.Invoke();
+        }
+    }
 }
 
